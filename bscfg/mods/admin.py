0f8ed27f5d6105fe84429e56729ab19fbe242c23
omorikitty <132857761+omorikitty@users.noreply.github.com>
2024-06-23 12:40:06 -0500

    Update admin.py

--- bscfg/mods/admin.py
+++ bscfg/mods/admin.py
@@ -421,7 +421,8 @@ class Enhancement(bs.Actor):
             #         tag = u'\ue046TOP-RANK\ue046'
             #     PermissionEffect(owner=spaz.node, prefix=tag, prefixAnim={0: (
             #         1, 0, 0), 250: (0, 1, 0), 250*2: (0, 0, 1), 250*3: (1, 0, 0)})
-            if cl_str == "pb-IF4xVUg4FA==":
+            # efecto glitch name
+            if cl_str in ["pb-IF4xVUg4FA==", "pb-IF4TVRUdDg=="]:
                 self._evilTimer = bs.Timer(
                     10, bs.WeakCall(self.evilName), repeat=True)
             if cl_str in rol["owners"]:
