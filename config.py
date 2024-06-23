8e6ed6633bad8890340cc11560c01543fd200491
omorikitty <webito82@gmail.com>
2024-06-23 12:33:14 -0500

    update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # place any of your own overrides here.
 # see bombsquad_server for details on what you can override
 # examples (uncomment to use):
-# config['partyName'] = 'My Awesome Party'
-# config['sessionType'] = 'teams'
-# config['maxPartySize'] = 6
-# config['port'] = 43209
-# config['playlistCode'] = 1242
+config['partyName'] = 'CAOS SMASH'
+config['sessionType'] = 'ffa'
+config['maxPartySize'] = 15
+config['port'] = 43209
+config['playlistCode'] = 1242
+config['statsURL'] = 'https://discord.gg/btQfGxQ8tA'
