6232dbbea4d173d0a06d2753b6aac6b2d722fc99
Robel <robel.salgado@unah.hn>
2024-07-12 21:55:12 -0600

    update info

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # place any of your own overrides here.
 # see bombsquad_server for details on what you can override
 # examples (uncomment to use):
-config['partyName'] = 'CAOS SMASH'
+config['partyName'] = u'\ud83d\udca5CAOS SUPER MASH FFA\ud83d\udca5'
 config['sessionType'] = 'ffa'
 config['maxPartySize'] = 15
-config['port'] = 43209
-config['playlistCode'] = 1242
-config['statsURL'] = 'https://discord.gg/btQfGxQ8tA'
+config['port'] = 43210
+config['playlistCode'] = 434248
+config['statsURL'] = 'https://discord.gg/8b858RBdEy'
